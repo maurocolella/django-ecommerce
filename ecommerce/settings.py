bd518f88e0f977de837823182eb6735772edef07
Mauro Colella <mauro.ludovico.colella@gmail.com>
2023-02-27 19:39:21 +0800

    Implement products functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend.apps.FrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
+
